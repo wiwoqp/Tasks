54ea1a332431aa57b471e389934e6418e838e13d
wiwoqp <ilyaatrusovv@gmail.com>
2025-10-27 20:49:40 +0300

    done(maybe)

--- car/car.py
+++ car/car.py
@@ -15,59 +15,64 @@ class Car:
         self.fuel_fill_counter = 0 # сколько раз заправлялись
         self.how_much_spent_fuel = 0  # сколько потратили на заправку
 
-        self.for_second_while = 0  # переменная для второго внутреннего цикла
+        self.mileage_last_overhaul = 0  # пробег с последним кап ремонта
         self.tachograph_each_1000 = 0  # внутренний тахограф на 1000 км
-        self.gas_tank_copy = self.gas_tank # копия данных объема топлива
-        self.total_major_repair = 0
-        self.max_mileage_limit_copy = self.max_mileage_limit
+        self.gas_station = self.gas_tank # копия данных объема топлива
+        self.total_major_repair = 0  # общая стоимость кап ремонтов
 
     def drive(self):
         while self.way > 0:
-            while self.for_second_while < self.max_mileage_limit:
+            while self.mileage_last_overhaul < self.max_mileage_limit:
                 while self.gas_tank > 0:
                     self.__tachograph += 100 / self.fuel_consumption # добавляем километраж за один литр топлива
                     self.tachograph_each_1000 += 100 / self.fuel_consumption
                     self.gas_tank -= 1 # минус один литр
                     self.way -= 100 / self.fuel_consumption  # отнимаем расстояние от общего маршрута
-                    self.for_second_while += 100 / self.fuel_consumption # переменная для цикла
+                    self.mileage_last_overhaul += 100 / self.fuel_consumption # переменная для цикла
                     if self.tachograph_each_1000 >= 1000: # каждые 1000 км уменьшаем стоимость увеличиваем расход на 1%
                         self.price -= self.top_down_cost
                         self.fuel_consumption *= 1.01
                         self.tachograph_each_1000 = 0
                 # Заправка
                 self.fuel_fill_counter += 1  # количество заправок
-                self.how_much_spent_fuel += self.gas_tank_copy * self.cost_fuel # сколько тратим
-                self.gas_tank = self.gas_tank_copy # заливаем бак
+                self.how_much_spent_fuel += self.gas_station * self.cost_fuel # сколько тратим
+                self.gas_tank = self.gas_station # заливаем бак
 
 
             # Капитальный ремонт
             self.total_major_repair += self.major_repair
-            self.for_second_while = 0
+            self.mileage_last_overhaul = 0
 
+    @property
     def info(self):
-        return (self.__tachograph,  # Пробег
-                self.price,  # Остаточная стоимость
-                self.how_much_spent_fuel,  # Сколько было потрачено на топливо
-                self.fuel_fill_counter, # Количество заправок
-                self.max_mileage_limit - self.for_second_while,  # сколько пробега осталось до кап ремонта
-                self.total_major_repair) # общая стоимость еап ремонта
+        return {
+            "mileage": round(self.__tachograph),
+            "price": self.price,
+            "fuel_spent": self.how_much_spent_fuel,
+            "refuels": self.fuel_fill_counter,
+            "to_repair": self.max_mileage_limit - self.mileage_last_overhaul,
+            "repairs_cost": self.total_major_repair
+        }
+
 
 
 cars = []
+
+total_price = 0
 for i in range(1, 101):
     car = Car(i)
     car.drive()
     cars.append(car)
+    total_price += car.price
 
 diesel_cars = [i for i in cars if i.cost_fuel == 1.8]
 petrol_cars = [i for i in cars if i.cost_fuel == 2.4]
 
-diesel_sorted = sorted(diesel_cars, key=lambda c: c.info()[1], reverse=True)
-petrol_sorted = sorted(petrol_cars, key=lambda c: c.info()[4], reverse=True)
+diesel_sorted = sorted(diesel_cars, key=lambda c: c.info["price"], reverse=True)
+petrol_sorted = sorted(petrol_cars, key=lambda c: c.info["to_repair"], reverse=True)
 
-# суммарная стоимость автопарка
-total_value = sum(c.info()[1] for c in cars)
 
+print(total_price)
 
 
 
